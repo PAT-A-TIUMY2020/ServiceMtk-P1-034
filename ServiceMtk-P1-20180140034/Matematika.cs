f236efecb53169181e356bc4c19ae08f5aef480c
Aalfinuraziz <alfinuraziz885@gmail.com>
2020-11-09 19:36:53 +0700

    exception handling server

--- ServiceMtk-P1-20180140034/Matematika.cs
+++ ServiceMtk-P1-20180140034/Matematika.cs
@@ -13,29 +13,108 @@ namespace ServiceMtk_P1_20180140034
     {
         public int Bagi(int a, int b)
         {
-            return a / b;
+            try
+            {
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                //secara Default
+                //throw new Exception(ex.Message) //pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang di kirim ke client
+
+                MathFault mf = new MathFault();
+                mf.kode = "Input Yang Anda Masukkan Salah";
+                mf.pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         public int Kali(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                //secara Default
+                //throw new Exception(ex.Message) //pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang di kirim ke client
+
+                MathFault mf = new MathFault();
+                mf.kode = "Input Yang Anda Masukkan Salah";
+                mf.pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         public int Kurang(int a, int b)
         {
-            return a - b;
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                //secara Default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang di kirim ke client
+
+                MathFault mf = new MathFault();
+                mf.kode = "Input Yang Anda Masukkan Salah";
+                mf.pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         public int Tambah(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                //secara Default
+                //throw new Exception(ex.Message) //pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang di kirim ke client
+
+                MathFault mf = new MathFault();
+                mf.kode = "Input Yang Anda Masukkan Salah";
+                mf.pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         //TKoordinat:nama methodnya, a,b = input seperti int.
         //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil
         public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
-            Koordinat hasil = new Koordinat();
-            hasil.X = a.X + b.X;
-            hasil.Y = a.Y + b.Y;
-            return hasil;
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
 
+                //secara Default
+                //throw new Exception(ex.Message) //pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang di kirim ke client
+
+                MathFault mf = new MathFault();
+                mf.kode = "Input Yang Anda Masukkan Salah";
+                mf.pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
     }
 }
